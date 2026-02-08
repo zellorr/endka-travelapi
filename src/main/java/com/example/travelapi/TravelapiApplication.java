c8a4e33cc0dea90d6f04cab5cee839994ee83de6
Ulan Akhmet <ulanahmet5@gmail.com>
2026-02-08 17:58:26 +0500

    Initial commit

---
+++ src/main/java/com/example/travelapi/TravelapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelapiApplication.class, args);
+	}
+
+}
